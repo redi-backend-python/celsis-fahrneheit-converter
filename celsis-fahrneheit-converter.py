03e13863c684bf0cb10b440337898aa7d6d39787
luca <luca@pop-os.localdomain>
2020-09-06 22:22:56 +0200

    Add error handling on input.

--- celsis-fahrneheit-converter.py
+++ celsis-fahrneheit-converter.py
@@ -8,12 +8,20 @@ def print_celsius_values_with_signature(celsius):
 
 
 def compute_fahrenheit(celsius):
-    result = 9/5 * celsius + 32
+    result = 9 / 5 * celsius + 32
     return result
 
 
+def parse_input():
+    try:
+        return float(input("Enter a Temperature in Celsius to convert it to Fahrenheit: \n"))
+    except ValueError as e:
+        print("--- ZYou have entered an invalid number. ---")
+        exit(-1)
+
+
 def main():
-    celsius = float(input("Enter a Temperature in Celsius to convert it to Fahrenheit: \n"))
+    celsius = parse_input()
     print_celsius_values_with_signature(celsius)
     fahrenheit = compute_fahrenheit(celsius)
     print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' % (celsius, fahrenheit))
