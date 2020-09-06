920f622c896a2cd8feed0a07f387180032d17511
luca <luca@pop-os.localdomain>
2020-09-06 22:22:02 +0200

    Add solved exercise.

--- celsis-fahrneheit-converter.py
+++ celsis-fahrneheit-converter.py
@@ -1,19 +1,19 @@
 def print_celsius_values_with_signature(celsius):
-    if False:  # Please add the condition
+    if celsius > 0:
         print("The celsius value you entered is positive")
-    elif False:  # Please add the condition
+    elif celsius == 0:
         print("The celsius value you entered is zero")
     else:
         print("The celsius value you entered is negative")
 
 
 def compute_fahrenheit(celsius):
-    result = 0 # Please calculate fahrenheit
+    result = 9/5 * celsius + 32
     return result
 
 
 def main():
-    celsius = float(input("Enter a Temperature in Celsius to convert it to Fahrenheit: "))
+    celsius = float(input("Enter a Temperature in Celsius to convert it to Fahrenheit: \n"))
     print_celsius_values_with_signature(celsius)
     fahrenheit = compute_fahrenheit(celsius)
     print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' % (celsius, fahrenheit))
